daaad3a515481d59da8ced37d0bb2696f161ef18
MJBarbosa <marionjericobarbosa@gmail.com>
2023-10-27 11:47:28 +0800

    graph done thank you Lord

--- app/src/main/java/com/example/idealist/CustomerStoreProductActivity.java
+++ app/src/main/java/com/example/idealist/CustomerStoreProductActivity.java
@@ -10,7 +10,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
--- app/src/main/java/com/example/idealist/MainSOActivity.java
+++ app/src/main/java/com/example/idealist/MainSOActivity.java
@@ -24,7 +24,26 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+import com.jjoe64.graphview.DefaultLabelFormatter;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.GridLabelRenderer;
+import com.jjoe64.graphview.Viewport;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.DataPointInterface;
+import com.jjoe64.graphview.series.LineGraphSeries;
+import com.jjoe64.graphview.series.OnDataPointTapListener;
+import com.jjoe64.graphview.series.Series;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
 public class MainSOActivity extends AppCompatActivity {
 
@@ -35,6 +54,15 @@ public class MainSOActivity extends AppCompatActivity {
     private NavigationView navigationView;
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle drawerToggle;
+    private DatabaseReference salesDataRef;
+    private GraphView salesGraph;
+    private LineGraphSeries<DataPoint> series;
+    private long twelveMonthsInMillis;
+    private int currentYear;
+    private int maxDataPoints = 12;
+    private double maxSalesValue = 0.0;
+    // Declare the dataPoints list as a class member
+    private List<DataPoint> dataPoints = new ArrayList<>();
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
@@ -52,6 +80,28 @@ public class MainSOActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("Main SO Activity");
 
         swipeToRefresh();
+
+        // Initialize the GraphView
+        salesGraph = findViewById(R.id.salesGraph);
+
+        // Add a title to the graph
+        salesGraph.setTitle("Sales Over Time");
+
+        series = new LineGraphSeries<>();
+        salesGraph.addSeries(series);
+
+        // Set the OnDataPointTapListener to display details when a point is tapped
+        series.setOnDataPointTapListener(new OnDataPointTapListener() {
+            @Override
+            public void onTap(Series series, DataPointInterface dataPoint) {
+                // Handle tap event, e.g., display details of the data point
+                String details = "Timestamp: " + new Date((long) dataPoint.getX()) + "\nSales: " + dataPoint.getY();
+                Toast.makeText(MainSOActivity.this, details, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Initialize Firebase database reference
+        salesDataRef = FirebaseDatabase.getInstance().getReference().child("Sales");
 
         authProfileSO = FirebaseAuth.getInstance();
         textViewSO = findViewById(R.id.textViewSO);
@@ -90,6 +140,12 @@ public class MainSOActivity extends AppCompatActivity {
                 return true;
             }
         });
+
+        // Calculate twelveMonthsInMillis once
+        twelveMonthsInMillis = 12L * 30L * 24L * 60L * 60L * 1000L;
+
+        // Initialize the graph
+        initializeGraph();
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setSelectedItemId(R.id.menu_home_so);
@@ -135,6 +191,193 @@ public class MainSOActivity extends AppCompatActivity {
             textViewSO.setText(firebaseUserSO.getEmail());
         }
     }
+
+    private void initializeGraph() {
+        Viewport viewport = salesGraph.getViewport();
+        viewport.setYAxisBoundsManual(true);
+        viewport.setXAxisBoundsManual(true);
+
+        long currentTimestamp = System.currentTimeMillis();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(currentTimestamp);
+        currentYear = calendar.get(Calendar.YEAR);
+
+        // Set minX to the beginning of the current year (January 1st, current year)
+        calendar.set(currentYear, 0, 1, 0, 0, 0);
+        long minX = calendar.getTimeInMillis();
+
+        // Set maxX to the end of the current year (December 31st, current year)
+        calendar.set(currentYear, 11, 31, 23, 59, 59);
+        long maxX = calendar.getTimeInMillis();
+
+        // Calculate the number of visible months based on screen width
+        int maxDataPoints = 12;
+        int pixelsPerDataPoint = (int) (maxX - minX) / maxDataPoints;
+        int maxVisibleRange = maxDataPoints;
+
+        // Calculate the minimum visible points based on screen width
+        int minDataPoints = maxVisibleRange;
+
+        // Disable scrolling
+        viewport.setScalable(false);
+        viewport.setScrollable(false);
+
+        // Set the calculated bounds
+        viewport.setMaxX(maxX);
+        viewport.setMinX(minX);
+        viewport.setMaxY(calculateMaxSalesValue());
+
+        // Implement a custom X-axis label formatter
+        GridLabelRenderer gridLabel = salesGraph.getGridLabelRenderer();
+        gridLabel.setHorizontalLabelsVisible(true);
+        gridLabel.setHorizontalAxisTitle("Months " + currentYear);
+
+        gridLabel.setVerticalLabelsVisible(true);
+        gridLabel.setVerticalAxisTitle("Number of Items");
+
+        // Set the custom X-axis label formatter
+        gridLabel.setLabelFormatter(new DefaultLabelFormatter() {
+            @Override
+            public String formatLabel(double value, boolean isValueX) {
+                if (isValueX) {
+                    return formatXLabel((long) value);
+                } else {
+                    // Format Y-axis labels here
+                    return super.formatLabel(value, isValueX);
+                }
+            }
+        });
+
+        // Initialize the graph with data points by loading sales data for each month
+        for (int month = 1; month <= 12; month++) {
+            loadSalesDataForYearAndMonth(currentYear, month);
+        }
+    }
+
+    private void loadSalesDataForYearAndMonth(int year, int month) {
+        Log.d(TAG, "Start of loadSalesDataForYearAndMonth");
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month - 1, 1, 0, 0, 0);
+        long startTimestamp = calendar.getTimeInMillis();
+
+        calendar.set(Calendar.DATE, calendar.getActualMaximum(Calendar.DATE));
+        calendar.set(Calendar.HOUR_OF_DAY, 23);
+        calendar.set(Calendar.MINUTE, 59);
+        calendar.set(Calendar.SECOND, 59);
+        long endTimestamp = calendar.getTimeInMillis();
+
+        Log.d(TAG, "Database for sale: " + salesDataRef);
+
+        Query query = salesDataRef
+                .orderByChild("timestamp");
+
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Log.d(TAG, "Number of children in dataSnapshot: " + dataSnapshot.getChildrenCount());
+
+                Log.d(TAG, "Fetching data for year: " + year + ", month: " + month);
+
+                List<DataPoint> dataPointsForMonth = new ArrayList<>(); // Create a list to store data points for the current month
+
+                String currentUserUid = firebaseUserSO.getUid(); // Get the current user's UID
+
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    String saleUserUid = snapshot.child("userUid").getValue(String.class);
+                    Double saleAmount = snapshot.child("price").getValue(Double.class);
+                    String saleTimestampStr = snapshot.child("timestamp").getValue(String.class);
+
+                    if (saleUserUid != null && saleUserUid.equals(currentUserUid)) {
+                        if (saleAmount != null && saleTimestampStr != null && !saleTimestampStr.isEmpty()) {
+                            long saleTimestamp = parseTimestamp(saleTimestampStr);
+                            if (saleTimestamp >= startTimestamp && saleTimestamp <= endTimestamp) {
+                                // Add each data point to the list for the current month
+                                dataPointsForMonth.add(new DataPoint(saleTimestamp, saleAmount));
+                                Log.d(TAG, "Data fetched for sales: timestamp: " + saleTimestampStr + saleAmount + saleUserUid);
+                            }
+                        }
+                    }
+                }
+
+                // Add data points for the current month to the dataPoints list
+                dataPoints.addAll(dataPointsForMonth);
+
+                // Add data points for the current month to the series
+                series.resetData(dataPoints.toArray(new DataPoint[0]));
+
+
+                // Update the maximum sales value if necessary
+                maxSalesValue = Math.max(maxSalesValue, calculateMaxSalesValue());
+
+                // Update the graph view
+                updateGraphView();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Handle database error
+                Log.e(TAG, "Database error: " + databaseError.getMessage());
+            }
+        });
+        Log.d(TAG, "End of loadSalesDataForYearAndMonth");
+    }
+
+    private String formatXLabel(long timestamp) {
+        // Convert the timestamp to a readable date format with month names
+        SimpleDateFormat sdf = new SimpleDateFormat("MMM");
+        return sdf.format(new Date(timestamp));
+    }
+
+    private void updateGraphView() {
+        // Set the maximum Y value for the graph
+        salesGraph.getViewport().setMaxY(maxSalesValue);
+
+        // Update the graph with the series containing data points
+        salesGraph.removeAllSeries();
+        salesGraph.addSeries(series);
+
+        // Set the custom X-axis label formatter (moved from initializeGraph)
+        GridLabelRenderer gridLabel = salesGraph.getGridLabelRenderer();
+        gridLabel.setHorizontalLabelsVisible(true);
+        gridLabel.setHorizontalAxisTitle("Months " + currentYear);
+
+        gridLabel.setVerticalLabelsVisible(true);
+        gridLabel.setVerticalAxisTitle("Number of Items");
+    }
+
+    private double calculateMaxSalesValue() {
+        // Initialize a variable to keep track of the maximum sales value
+        double maxSalesValue = 0.0;
+
+        // Iterate through the data points to find the maximum sales value
+        for (DataPoint dataPoint : dataPoints) {
+            double salesValue = dataPoint.getY();
+            if (salesValue > maxSalesValue) {
+                maxSalesValue = salesValue;
+            }
+        }
+
+        // Add some padding to the maximum value for a better graph display
+        maxSalesValue *= 1.2; // Adjust the padding factor as needed
+
+        return maxSalesValue;
+    }
+
+    // Other methods as needed
+
+    private long parseTimestamp(String timestampStr) {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US);
+        try {
+            Date date = format.parse(timestampStr);
+            if (date != null) {
+                return date.getTime();
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return 0; // Return 0 if parsing fails
+    }
+
 
     private void swipeToRefresh() {
         swipeContainer = findViewById(R.id.swipeContainer);
